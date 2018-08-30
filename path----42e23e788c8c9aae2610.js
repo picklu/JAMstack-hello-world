dc956b97de56465929df3eec9f7324622d3b3ac1
Subrata Sarker <subrata_sarker@yahoo.com>
2018-08-30 09:14:41 +0000

    Updates

---
+++ path----42e23e788c8c9aae2610.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"JAMstack Hello World"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----42e23e788c8c9aae2610.js.map
